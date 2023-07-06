9ef93510d8251dca1cf3e84bbe94deb8abe2e64b
Ville Juutila <ville.juutila@gmail.com>
2023-07-06 12:07:19 +0300

    Added CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,24 @@
 export default [
   "strapi::errors",
-  "strapi::security",
+  { 
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://staging-valon-kaupunki.s3.us-east-2.amazonaws.com",
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
